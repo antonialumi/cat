5d94a682d3e1a679ed580a06621c369cef452f72
Antonia Lumi Alberti <antonialumialberti@gmail.com>
2023-09-21 22:00:38 -0300

    adding a button to go back to the home

--- pages/letsgo.tsx
+++ pages/letsgo.tsx
@@ -1,3 +1,33 @@
-export default function letsgo(){
-    return <>AAAAAAAA letsgo</>
+import { useState } from "react";
+import Home from ".";
+
+
+const randomWords = ["potato","spaghetti","cat","clock"];
+
+
+
+export default function Letsgo(){
+    const [selectedPhrase,setSelectedPhrase] = useState('hola')
+
+    function giveMeAWord(){
+        const randomNumber = Math.floor(Math.random() * randomWords.length)
+        return randomWords[randomNumber]
+    }
+
+    const pressWordButton = () => {
+        console.log('button was pressed')
+        setSelectedPhrase(giveMeAWord())
+    }
+        
+
+    return (<>
+        <div className="font-sans" >AAAAAAAA letsgo</div> <br/>
+        <button onClick={ e => pressWordButton()}>
+            Word
+        </button> <br/>
+        {selectedPhrase}
+        <br />
+        <a href="/">Home</a>
+    </>)
+
 }
