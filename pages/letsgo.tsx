2ab0e1bd02bfa8c0d8b0952117be0b5799f4fefa
Antonia Lumi Alberti <antonialumialberti@gmail.com>
2023-10-04 18:43:09 -0300

    just added a img (drawing by me) and a function that asigns a picture to each random word

--- pages/letsgo.tsx
+++ pages/letsgo.tsx
@@ -1,6 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 import { useState } from "react";
 import Link from "next/link";
-
 
 
 const randomWords = ["potato","spaghetti","cat","clock"];
@@ -8,7 +8,15 @@ const randomWords = ["potato","spaghetti","cat","clock"];
 
 
 export default function Letsgo(){
-    const [selectedPhrase,setSelectedPhrase] = useState('hola')
+    const [selectedPhrase,setSelectedPhrase] = useState(' ')
+    const [showImage, setShowImage] = useState(false);
+
+    const wordImageMapping: Record<string, string> = {
+        potato: 'potato.jpg',
+        spaghetti: 'spaghetti.jpg',
+        cat: '/media/cat.png',
+        clock: 'clock.jpg',
+      };
 
     function giveMeAWord(){
         const randomNumber = Math.floor(Math.random() * randomWords.length)
@@ -16,20 +24,35 @@ export default function Letsgo(){
     }
 
     const pressWordButton = () => {
-        console.log('button was pressed')
-        setSelectedPhrase(giveMeAWord())
-    }
-        
+        console.log('button was pressed');
+        const word = giveMeAWord();
+        setSelectedPhrase(word);
+
+        if(word in wordImageMapping){
+            setShowImage(true);
+        }
+        setShowImage(false)
+        }
 
     return (
         <><div className="fondo" style={{ minHeight: '100vh' }}>
             <div className="motherbox">
-                <h1 className="box">AAAAAAAA letsgo</h1>
-                <div className="box">
+            <div className="box">
+                    {selectedPhrase}
+                    <br />
                     <button onClick={e => pressWordButton()}>
-                        Word
-                    </button>
-                    <div>{selectedPhrase}</div>
+                    Press to get a fact
+                    </button> 
+                    <br />
+                    <div> 
+            </div>
+                </div>
+                <div className="box"style={{ display: 'flex',justifyContent: 'center', alignItems: 'center' }}>
+                    <img
+                    src={wordImageMapping[selectedPhrase]}
+                    alt={selectedPhrase}
+                    style={{ width: '360px', height: 'auto', alignContent: 'center', padding: '0px', margin:'0px' }} 
+                  />
                 </div>
                 <br />
                 </div>
