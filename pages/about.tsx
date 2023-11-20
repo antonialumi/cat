2078ecd60a099e93f317d4cfe7aef4cb887d7513
Antonia Lumi Alberti <antonialumialberti@gmail.com>
2023-11-20 15:04:17 -0300

    polishing some things

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+export default function About() {
+    return <>
+        <h1 style={{textAlign:'center', fontFamily:'Bebas Neue', fontSize: 25,}} >Hi! my name is Antonia, im a tech project coordinator and initiate programmer based in argentina</h1><br/>
+        <br/>
+<div style={{textAlign:'center', fontFamily: 'Bebas Neue'}}>
+    <a href="https://www.linkedin.com/in/antonia-lumi-alberti-176350199/">LinkedIn</a>
+        <br />
+        <a href="https://github.com/antonialumi">GitHub</a>
+    
+    </div>
+    </>    
+
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,7 @@ export default function Home() {
   <>
   <div className="fondo" style={{ minHeight: '100vh' }}>
       <div className="motherbox">
-        <h1 className="box">Hello</h1>
-        <h2 className="box">aaaaa</h2>
+    <h1 style={{textAlign:'left', fontFamily:'Bebas Neue', fontSize: '50px' }}>This is a Random fact generator, but cat themed. Click LETS GO to check it out!</h1>
       </div>
     </div></>
   )
